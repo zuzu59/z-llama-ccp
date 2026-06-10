344aae1cf1eec4f38d05ad1d235903da8b412ae0
Christian Zufferey <christian@zufferey.com>
2026-06-10 09:31:53 +0200

    refact: corrigé un modèle dans la proposition de download dans le commentaire

--- install-huggingface_hub.sh
+++ install-huggingface_hub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Petit script pour installer huggingface_hub
-#zf260609.1609, zf260610.0919
+#zf260609.1609, zf260610.0931
 
 # source: https://www.reddit.com/r/LocalLLaMA/comments/1typjmc/120_toks_on_12gb_vram_with_gemma_4_12b_qat_mtp/?show=original
 
@@ -18,7 +18,7 @@ hf auth login
 echo -e "
 Pour descendre les modèles:
 
-hf download google/gemma-4-12B-it-qat-q4_0-gguf gemma-4-12B-it-Q4_0.gguf --local-dir .
+hf download google/gemma-4-12B-it-qat-q4_0-gguf gemma-4-12b-it-qat-q4_0.gguf --local-dir .
 
 hf download unsloth/gemma-4-12B-it-qat-GGUF gemma-4-12B-it-qat-UD-Q4_K_XL.gguf --local-dir .
 hf download Janvitos/gemma-4-12B-it-qat-assistant-MTP-Q8_0-GGUF gemma-4-12B-it-qat-assistant-MTP-Q8_0.gguf --local-dir .
