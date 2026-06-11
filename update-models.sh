e2d670432f6ecd97a0e03138ae0b744344cabcb5
Christian Zufferey <christian@zufferey.com>
2026-06-11 09:44:05 +0200

    new: ajouté le modèle gemma4:12b QAT original de Google au lieu de Unsloth

--- update-models.sh
+++ update-models.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Petit script pour passer les updates de modèles huggingface_hub
-#zf260609.1609, zf260610.0926
+#zf260609.1609, zf260611.0931
 
 # source: https://www.reddit.com/r/LocalLLaMA/comments/1typjmc/120_toks_on_12gb_vram_with_gemma_4_12b_qat_mtp/?show=original
 
@@ -8,8 +8,13 @@ cd ~/dev/llama.cpp
 
 source .venv/bin/activate
 
-hf download google/gemma-4-12B-it-qat-q4_0-gguf gemma-4-12b-it-qat-q4_0.gguf --local-dir .
 
+# Pour les originaux de Goole
+hf download google/gemma-4-12B-it-qat-q4_0-gguf gemma-4-12b-it-qat-q4_0.gguf --local-dir .
+hf download google/gemma-4-12B-it-qat-q4_0-gguf mmproj-gemma-4-12b-it-qat-q4_0.gguf --local-dir .
+
+
+# Pour les modified de Unsolth
 hf download unsloth/gemma-4-12B-it-qat-GGUF gemma-4-12B-it-qat-UD-Q4_K_XL.gguf --local-dir .
 hf download Janvitos/gemma-4-12B-it-qat-assistant-MTP-Q8_0-GGUF gemma-4-12B-it-qat-assistant-MTP-Q8_0.gguf --local-dir .
 
