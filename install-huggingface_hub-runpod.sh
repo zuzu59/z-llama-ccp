ebb2a02d36b3941ca697efcc3e4bab4924ecfca1
Christian Zufferey <christian@zufferey.com>
2026-08-04 13:42:55 +0000

    fixe: enfin cela fonctionne \o/

--- install-huggingface_hub-runpod.sh
+++ install-huggingface_hub-runpod.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Petit script pour installer huggingface_hub
-#zf260609.1609, zf260804.1441
+#zf260609.1609, zf260804.1536
 
 # source: https://www.reddit.com/r/LocalLLaMA/comments/1typjmc/120_toks_on_12gb_vram_with_gemma_4_12b_qat_mtp/?show=original
 
@@ -27,14 +27,21 @@ hf download Janvitos/gemma-4-12B-it-qat-assistant-MTP-Q8_0-GGUF gemma-4-12B-it-q
 
 
 
-cd /workspace/llama.cpp
-source .venv/bin/activate
-alias huggingface-cli=hf
-mkdir /workspace/models
+cd /workspace/llama.cpp ; source .venv/bin/activate ; alias huggingface-cli=hf
+
+#mkdir /workspace/models
 
 
 huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-9B-GGUF deepreinforce-ai_Ornith-1.0-9B-Q8_0.gguf --local-dir /workspace/models
 huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-9B-GGUF mmproj-deepreinforce-ai_Ornith-1.0-9B-bf16.gguf --local-dir /workspace/models
+
+huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-35B-GGUF deepreinforce-ai_Ornith-1.0-35B-Q4_K_M.gguf --local-dir /workspace/models
+huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-35B-GGUF mmproj-deepreinforce-ai_Ornith-1.0-35B-bf16.gguf --local-dir /workspace/models
+
+
+huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-35B-GGUF --include "qwen2_vl_7b_instruct-mmproj-f16.gguf" --local-dir /workspace/models --local-dir-use-symlinks False
+
+
 
 
 exit
