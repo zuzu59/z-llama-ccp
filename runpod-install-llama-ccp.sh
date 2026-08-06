66e6b1eec8296c7ecb0b8c1229d0091911614a9f
Christian Zufferey <christian@zufferey.com>
2026-08-06 16:21:46 +0000

    fix: corrigé une sacrée coquille llama.ccp au lieu de llama.cpp :-(

--- runpod-install-llama-ccp.sh
+++ runpod-install-llama-ccp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#Petit script pour installer le serveur llama.ccp sur runpod en copiant directement, sans compiler, le binaire depuis github
+#Petit script pour installer le serveur llama.cpp sur runpod en copiant directement, sans compiler, le binaire depuis github
 #Cette astuce permet de gagner 20 minutes de copilation
-#zf260609.1606, zf260806.1805
+#zf260609.1606, zf260806.1817
 
 # source: https://github.com/ai-dock/llama.cpp-cuda/releases
 
@@ -10,7 +10,7 @@
 #set -v -x
 #set -v
 
-export FOLDER_LLAMA="/workspace/llama.ccp/build/bin"
+export FOLDER_LLAMA="/workspace/llama.cpp/build/bin"
 export VERSION_LLAMA="b10290"
 export URL_GITHUB_LLAMA="https://github.com/ai-dock/llama.cpp-cuda/releases/download/$VERSION_LLAMA/llama.cpp-$VERSION_LLAMA-cuda-12.8-amd64.tar.gz"
 
@@ -24,11 +24,11 @@ tar -xzf llama* --no-same-owner
 rm llama*
 mv cuda-12.8/* ./
 
-# superbe astuce de google ia pour avoir le bon path dans toutes les lib llama.ccp comme si on l'avait compilé sur cette machine
+# superbe astuce de google ia pour avoir le bon path dans toutes les lib llama.cpp comme si on l'avait compilé sur cette machine
 
 #apt-get update && apt-get install -y patchelf
 
-for f in /workspace/llama.ccp/build/bin/*; do
+for f in $FOLDER_LLAMA/*; do
     if [ ! -d "$f" ]; then
         echo "Patch du RPATH ($ORIGIN) pour : $(basename "$f")"
         patchelf --force-rpath --set-rpath '$ORIGIN' "$f"
@@ -40,7 +40,7 @@ cd ../..
 pwd
 
 echo -e "
-llama.ccp installé !
+llama.cpp installé !
 "
 
 ./build/bin/llama-cli --list-devices
