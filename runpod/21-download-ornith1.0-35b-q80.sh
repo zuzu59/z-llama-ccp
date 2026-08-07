29cfacb5631b419a3b249f2301b4b77b32ffe620
Christian Zufferey <christian@zufferey.com>
2026-08-07 13:01:15 +0000

    new: ajouté le modèle ornith 35b q80

---
+++ runpod/21-download-ornith1.0-35b-q80.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#Petit script pour descendre sur runpode le modèle ornith1.0-35b-q80 depuis huggingface_hub
+#zf260609.1609, zf260807.1455
+
+# source: https://www.reddit.com/r/LocalLLaMA/comments/1typjmc/120_toks_on_12gb_vram_with_gemma_4_12b_qat_mtp/?show=original
+
+#set -e -v -x
+#set -v
+
+
+cd /workspace/llama.cpp/
+source .venv/bin/activate
+
+shopt -s expand_aliases # Active les alias dans les scripts
+alias huggingface-cli=hf
+
+huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-35B-GGUF deepreinforce-ai_Ornith-1.0-35B-Q8_0.gguf --local-dir /workspace/models
+huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-35B-GGUF mmproj-deepreinforce-ai_Ornith-1.0-35B-bf16.gguf --local-dir /workspace/models
+
