454830691bdc09d177268938e0d6d673fcadaa1b
Christian Zufferey <christian@zufferey.com>
2026-08-17 12:31:02 +0000

    new: ajouté le modèle Q6_K_L

---
+++ runpod/21-download-qwen3.8-27b-q6kl.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#Petit script pour descendre sur runpode le modèle qwen 3.8:27b depuis huggingface_hub
+#zf260609.1609, zf260817.1419
+
+# source: https://www.reddit.com/r/LocalLLaMA/comments/1typjmc/120_toks_on_12gb_vram_with_gemma_4_12b_qat_mtp/?show=original
+
+#set -e -v -x
+#set -v
+
+
+cd /workspace/llama.cpp/
+source .venv/bin/activate
+
+shopt -s expand_aliases # Active les alias dans les scripts
+alias huggingface-cli=hf
+
+# 1. Télécharger le fichier principal du modèle (Texte/Raisonnement)
+#huggingface-cli download prithivMLmods/Qwen3.8-27B-GGUF Qwen3.8-27B.Q8_0.gguf --local-dir /workspace/models
+#huggingface-cli download bartowski/Qwen3.8-27B-GGUF Qwen3.8-27B.Q8_0.gguf --local-dir /workspace/models
+
+hf download hf://bartowski/Qwen3.8-27B-GGUF/Qwen3.8-27B-Q6_K_L.gguf --local-dir /workspace/models
+
+
+
+
+# 2. Télécharger le projecteur visuel en version f16 (Multimodal / Vision)
+#huggingface-cli download prithivMLmods/Qwen3.8-27B-GGUF Qwen3.8-27B.mmproj-f16.gguf --local-dir /workspace/models
+huggingface-cli download bartowski/Qwen3.8-27B-GGUF mmproj-Qwen3.8-27B-f16.gguf --local-dir /workspace/models
+
+
---
+++ runpod/31-start-qwen3.8-27b-q6kl.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#Petit script pour démarrer le serveur llama.ccp avec le tout dernier qwen 3.8:27b avec vision
+#zf260608.1407, zf260817.1420
+
+# source: 
+
+# Download les modèles
+
+#alias huggingface-cli=hf
+#cd ~/dev/llama.cpp/
+#source .venv/bin/activate
+#huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-9B-GGUF deepreinforce-ai_Ornith-1.0-9B-Q8_0.gguf --local-dir ~/models
+#huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-9B-GGUF mmproj-deepreinforce-ai_Ornith-1.0-9B-bf16.gguf --local-dir ~/models
+
+
+
+/workspace/llama.cpp/build/bin/llama-server \
+  -m /workspace/models/Qwen3.8-27B-Q6_K_L.gguf \
+  --mmproj /workspace/models/mmproj-Qwen3.8-27B-f16.gguf \
+  --host 0.0.0.0 \
+  --port 8888 \
+  -ngl 99 \
+  -c 262144 \
+  -np 1 \
+  -ctk f16 \
+  -ctv f16 \
+  --flash-attn on \
+  -b 2048 \
+  -t 8 \
+  --image-min-tokens 1024 \
+  --spec-type draft-mtp \
+  --spec-draft-n-max 2
+
