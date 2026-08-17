09e153bb219968ba1ec7613e977c3ebadd3756d9
Christian Zufferey <christian@zufferey.com>
2026-08-17 12:02:46 +0000

    refact: amélioré le chargement mais ce n'est pas encore cela

--- runpod/31-start-qwen3.8-27b-80.sh
+++ runpod/31-start-qwen3.8-27b-80.sh
@@ -13,44 +13,22 @@
 #huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-9B-GGUF mmproj-deepreinforce-ai_Ornith-1.0-9B-bf16.gguf --local-dir ~/models
 
 
-cd /workspace/llama.cpp
 
-./build/bin/llama-server \
+
+/workspace/llama.cpp/build/bin/llama-server \
   -m /workspace/models/Qwen3.8-27B.Q8_0.gguf \
   --mmproj /workspace/models/Qwen3.8-27B.mmproj-f16.gguf \
   --host 0.0.0.0 \
   --port 8888 \
-  -ngl 90 \
-  -fit off \
+  -ngl 60 \
   -c 262144 \
   -np 1 \
   -ctk f16 \
   -ctv f16 \
   --flash-attn on \
   -b 2048 \
-  -t 8
-
-
-exit
-
-
-
-
-./build/bin/llama-server \
-  -m /workspace/models/deepreinforce-ai_Ornith-1.0-35B-Q8_0.gguf \
-  --mmproj /workspace/models/mmproj-deepreinforce-ai_Ornith-1.0-35B-bf16.gguf \
-  --host 0.0.0.0 \
-  --port 8888 \
-  -ngl 90 \
-  -fit off \
-  -c 262144 \
-  -np 1 \
-  -ctk f16 \
-  -ctv f16 \
-  --flash-attn on \
-  -b 2048 \
-  -t 8
-
-exit
-
+  -t 8 \
+  --image-min-tokens 1024 \
+  --spec-type draft-mtp \
+  --spec-draft-n-max 2
 
