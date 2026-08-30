4ad119b58e4e181852991150b3661ac8528fb051
Christian Zufferey <christian@zufferey.com>
2026-08-30 11:29:32 +0000

    new: nous script de démarrage pour l'ornit1.5

---
+++ runpod/31-start-ornith1.5-35b-q4km.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#Petit script pour démarrer le serveur llama.ccp avec le tout dernier ornith1.5:35b avec vision
+#zf260608.1407, zf260830.1316
+
+# source: 
+
+# Download les modèles
+
+#alias huggingface-cli=hf
+#cd ~/dev/llama.cpp/
+#source .venv/bin/activate
+#huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-9B-GGUF deepreinforce-ai_Ornith-1.0-9B-Q8_0.gguf --local-dir ~/models
+#huggingface-cli download bartowski/deepreinforce-ai_Ornith-1.0-9B-GGUF mmproj-deepreinforce-ai_Ornith-1.0-9B-bf16.gguf --local-dir ~/models
+
+
+cd /workspace/llama.cpp
+
+
+Ornith-1.5-35B-Q4_K_M.gguf
+mmproj-Ornith-1.5-35B-BF16.gguf
+
+
+
+./build/bin/llama-server \
+  -m /workspace/models/Ornith-1.5-35B-Q4_K_M.gguf \
+  --mmproj /workspace/models/mmproj-Ornith-1.5-35B-BF16.gguf \
+  --host 0.0.0.0 \
+  --port 8888 \
+  -ngl 99 \
+  -fit off \
+  -c 262144 \
+  -np 1 \
+  -ctk q4_0 \
+  -ctv q4_0 \
+  --flash-attn on \
+  -b 512 \
+  -t 8 \
+  --image-min-tokens 1024 \
+  --spec-type draft-mtp \
+  --spec-draft-n-max 2
+
+exit
+
+
