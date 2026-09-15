69b52d6db4e51e56bfffe4df39b05d01c59320c9
Christian Zufferey <christian@zufferey.com>
2026-09-15 10:04:40 +0000

    refact: utilise maintenant le modèle ornith 1.5

--- runpod/go-envoie-la-sauce.sh
+++ runpod/go-envoie-la-sauce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Petit script pour envoyer toute la sauce pour démarrer le modèle Ornith sur un pod de runpod
-#zf260807.1148
+#zf260915.1149
 
 #set -e -v -x
 #set -v
@@ -12,15 +12,19 @@ echo -e "\e[45m\e[97mClear l'installation...\e[0m\n"
 rm -rf /workspace/.cache/ /workspace/llama.cpp/ /workspace/models/
 
 
+echo -e "\e[45m\e[97mInstallation des utilitaires...\e[0m\n"
+./01-init-pod.sh
+
 echo -e "\e[45m\e[97mInstallation de llama.ccp...\e[0m\n"
 ./11-install-llama-ccp.sh
 
 echo -e "\e[45m\e[97mInstallation de huggingface_hub...\e[0m\n"
 ./12-install-huggingface_hub.sh
 
-echo -e "\e[45m\e[97mDownload-ornith1.0-35b-q4km...\e[0m\n"
-./21-download-ornith1.0-35b-q4km.sh
+echo -e "\e[45m\e[97mDownload-ornith1.5...\e[0m\n"
+#./21-download-ornith1.0-35b-q4km.sh
+./21-download-ornith1.5-35b-80.sh
 
-echo -e "\e[45m\e[97mStart-ornith1.0-35b-q4km...\e[0m\n"
-./31-start-ornith1.0-35b-q4km.sh
-
+echo -e "\e[45m\e[97mStart-ornith1.5...\e[0m\n"
+#./31-start-ornith1.0-35b-q4km.sh
+./31-start-ornith1.5-35b-80.sh
